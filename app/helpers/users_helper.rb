991d11dbebd9ea41d90fbf862eac922cb766846d
Adrian Corsini <glenflesk@gmail.com>
2013-11-16 22:21:56 +0000

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  #Returns the Gravatar (http://gravator.com/) for given user.
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravater_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravater_url, alt: user.name, class: "gravatar")
+  end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "Adrian Corsini"
+    email "cillarneyman1@hotmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
